b46bad638aeb02e581fee55e60b19fb71cc9ad88
Abdulaba <andresibanezulloa@gmail.com>
2024-10-23 13:19:24 -0300

    fetch movies basic and responsive

---
+++ v1/assets/js/index.js
@@ -0,0 +1,29 @@
+const url = 'http://www.omdbapi.com/?apikey=tuapikeys=';
+const button = document.querySelector('button');
+const cards = document.querySelector('.cards')
+const input = document.querySelector('input')
+
+const addMovieToDom = (movie) => {
+    const cardHTML = `
+        <div class="card"> 
+            <img src="${movie.Poster}" alt=""
+            <p id="year">${movie.Year}</p>
+        </div>
+    `;
+    
+    cards.insertAdjacentHTML('beforeend', cardHTML);
+}
+
+const movieIteration = (movies) => {
+    cards.innerHTML = ""
+    input.value = ""
+    movies.forEach(element =>  addMovieToDom(element));
+}
+
+const fetchMovie = (searchMovie) => {
+    fetch(url+searchMovie)
+        .then(resp => resp.json())
+        .then(({ Search }) => movieIteration(Search) );
+}
+
+button.addEventListener('click', () => fetchMovie(document.querySelector('#movie').value));
